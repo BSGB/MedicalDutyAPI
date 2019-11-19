4e99c1cba7baa4605c97c4ea37ba499f1e746c3c
Bartosz Spychalski <puntlik@outlook.com>
2019-11-19 23:54:05 +0100

    Initial commit, login/register & mockups
    Add-login controller with JWT generator (based on mockup data)
    Add-register controller with password hasher (based on mockup data)
    Add-user model

---
+++ MedicalDutyAPI/Controllers/LoginController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using MedicalDutyAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+
+namespace MedicalDutyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private static readonly string secretKey = "Tdt7J0zZfObeuL156DKhvcNGX3IslxEW";
+        private static readonly string tokenIssuer = "server";
+        private static readonly string tokenAudience = "server";
+        private static readonly int tokenValidMinutes = 15;
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Unauthorized("Login endpoint...");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(string email, string password)
+        {
+            var user = RegisterController.usersMockup.Find(user => user.Email == email);
+
+            if (user == null) return NotFound("No user found!");
+
+            var salt = Convert.FromBase64String(user.Salt);
+            var hashedPassword = RegisterController.HashPasswordPbkdf2(password, salt);
+
+            if (user.Password != hashedPassword) return BadRequest("Wrong password!");
+
+            var token = await CreateTokenAsync(user);
+
+            return Ok(token);
+        }
+
+        private static async Task<string> CreateTokenAsync(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var claimsIdentity = await CreateClaimsIdentityAsync(user);
+
+            var token = tokenHandler.CreateJwtSecurityToken
+                (
+                issuer: tokenIssuer,
+                audience: tokenAudience,
+                subject: claimsIdentity,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddMinutes(tokenValidMinutes),
+                signingCredentials: new SigningCredentials
+                (
+                    new SymmetricSecurityKey
+                    (
+                        Encoding.Default.GetBytes(secretKey)
+                    ),
+                    SecurityAlgorithms.HmacSha256Signature)
+                );
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        private static Task<ClaimsIdentity> CreateClaimsIdentityAsync(User user)
+        {
+            var claimsIdentity = new ClaimsIdentity();
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, user.FirstName));
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+
+            foreach (var role in user.Roles)
+            {
+                claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.ToString("d")));
+            }
+
+            return Task.FromResult(claimsIdentity);
+        }
+    }
+}
---
+++ MedicalDutyAPI/Controllers/RegisterController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MedicalDutyAPI.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace MedicalDutyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RegisterController : Controller
+    {
+        public static readonly List<User> usersMockup = new List<User>();
+
+        private static readonly int numBytesSalt = 128 / 8;
+        private static readonly int iterCount = 10000;
+        private static readonly int numBytesReq = 256 / 8;
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Unauthorized("Register endpoint...");
+        }
+
+        [HttpPost]
+        public ActionResult<User> Post(string firstName, string lastName, string password, string email)
+        {
+            var salt = GenerateSalt();
+            var hashedPassword = HashPasswordPbkdf2(password, salt);
+
+            var user = new User
+            {
+                Id = usersMockup.Count,
+                FirstName = firstName,
+                LastName = lastName,
+                Password = hashedPassword,
+                Salt = Convert.ToBase64String(salt),
+                Email = email,
+                Roles = new List<RoleEnum> { RoleEnum.Doctor },
+                CreatedAt = DateTimeOffset.Now.ToUnixTimeMilliseconds()
+            };
+
+            usersMockup.Add(user);
+
+            return Ok(user);
+        }
+
+        public static string HashPasswordPbkdf2(string password, byte[] salt)
+        {
+            var hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: iterCount,
+                numBytesRequested: numBytesReq
+            ));
+
+            return hashedPassword;
+        }
+
+        private static byte[] GenerateSalt()
+        {
+            var salt = new byte[numBytesSalt];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            return salt;
+        }
+    }
+}
